4fa7165ec6c33385d1893d73f2383cead26806ba
albert-ei-u <uwumuremyialbert5@gmail.com>
2026-03-29 23:07:21 +0200

    endpoints added

--- index.js
+++ index.js
@@ -7,11 +7,45 @@ const PORT = process.env.PORT || 3000;
 
 let users = [
     {id: 1, name: "Alice"},
-    {id:2, name: "Bob"}
+    {id:2, name: "Bob"}     
 ];
 
-app.get('/api/users', (req, res) => {
+app.get('/users', (req, res) => {
     res.json(users);
+});
+
+app.get('/users/:id', (req, res) => {
+    const user = users.find(u => u.id == req.params.id);
+    if(!user) return res.status(404).send('User not found');
+    res.json(user);
+});
+
+app.post('/users', (req, res) => {
+    const newUser = {}
+    newUser.id = users.length
+});
+
+//update user
+app.put('/users/:id', (req, res) => {
+    const user = users.find( u=> u.id == req.params.id);
+    if(!user) return res.status(404).send('User not found');
+    user.name = req.body.name;
+    res.json(user);
+});
+
+//delete user
+app.delete("/users/:id", (req,res) => {
+    users = users.filter(u => u.id != req.params.id);
+    res.send("user deleted")
+})
+
+app.post("/users", (req, res) => {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name
+    };
+    users.push(newUser);
+    res.status(201).json(newUser);
 });
 
 app.listen(PORT, () => {
